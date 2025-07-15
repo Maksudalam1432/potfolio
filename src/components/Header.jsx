2535be8a676ce172c1b97cfd143257df30351167
Maksud Alam <maksud.patna@gmail.com>
2025-07-16 00:43:17 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <ul className='flex justify-center gap-12'>
+        <li>home</li>
+        <li>skills</li>
+        <li>project</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
